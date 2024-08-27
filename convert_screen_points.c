0a3625171404bcb01884600dab9857781c043ef0
LaLaSero <filia2444@gmail.com>
2024-08-27 06:51:48 +0200

    add fov

--- convert_screen_points.c
+++ convert_screen_points.c
@@ -1,25 +1,25 @@
 #include "minirt.h"
 
-t_3d_vec	convert_screen_points(int x, int y, double fov)
-{
-	t_3d_vec	converted_point;
-
-	converted_point.x = (2.0 * (double)x) / ((double)WIDTH - 1) - 1.0;
-	converted_point.y = 1.0 - (2.0 * (double)y / (HEIGHT - 1));
-	converted_point.z = 0;
-	fov = 70;
-	return (converted_point);
-}
-
-// t_3d_vec convert_screen_points(int x, int y, double fov)
+// t_3d_vec	convert_screen_points(int x, int y, double fov)
 // {
-// 	t_3d_vec converted_point;
-// 	double aspect_ratio = (double)WIDTH / (double)HEIGHT;
-// 	double scale = tan(fov * 0.5 * 3.14159 / 180.0);
+// 	t_3d_vec	converted_point;
 
-// 	converted_point.x = (2.0 * (double)x / (double)WIDTH - 1.0) * aspect_ratio * scale;
-// 	converted_point.y = (1.0 - 2.0 * (double)y / (double)HEIGHT) * scale;
+// 	converted_point.x = (2.0 * (double)x) / ((double)WIDTH - 1) - 1.0;
+// 	converted_point.y = 1.0 - (2.0 * (double)y / (HEIGHT - 1));
 // 	converted_point.z = 0;
-
-// 	return converted_point;
+// 	fov = 70;
+// 	return (converted_point);
 // }
+
+t_3d_vec convert_screen_points(int x, int y, double fov)
+{
+	t_3d_vec converted_point;
+	double aspect_ratio = (double)WIDTH / (double)HEIGHT;
+	double scale = tan(fov * 0.5 * 3.14159 / 180.0);
+
+	converted_point.x = (2.0 * (double)x / (double)WIDTH - 1.0) * aspect_ratio * scale;
+	converted_point.y = (1.0 - 2.0 * (double)y / (double)HEIGHT) * scale;
+	converted_point.z = 0;
+
+	return converted_point;
+}
