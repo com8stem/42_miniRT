23f1e5392b46df01f7a2cc969804282218f1baa2
LaLaSero <filia2444@gmail.com>
2024-09-10 09:58:27 +0200

    parse subsub

--- parse_shape_sub.c
+++ parse_shape_sub.c
@@ -3,21 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   parse_shape_sub.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kishizu <kishizu@student.42.fr>            +#+  +:+       +#+        */
+/*   By: yutakagi <yutakagi@student.42.jp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 07:15:15 by yutakagi          #+#    #+#             */
-/*   Updated: 2024/09/10 16:57:51 by kishizu          ###   ########.fr       */
+/*   Updated: 2024/09/10 09:57:55 by yutakagi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
-
-void	check_color_range(int r, int g, int b)
-{
-	if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255)
-		config_error("Color range should be between 0 and 255");
-	return ;
-}
 
 static void	_is_valid_cy(t_rt_info *game, char **tmp, int cy_count,
 		t_cylinder tmp_cy)
--- parse_shape_sub_sub.c
+++ parse_shape_sub_sub.c
@@ -3,11 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   parse_shape_sub_sub.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kishizu <kishizu@student.42.fr>            +#+  +:+       +#+        */
+/*   By: yutakagi <yutakagi@student.42.jp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 16:51:56 by kishizu           #+#    #+#             */
-/*   Updated: 2024/09/10 17:12:04 by kishizu          ###   ########.fr       */
+/*   Updated: 2024/09/10 09:57:58 by yutakagi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
+
+void	check_color_range(int r, int g, int b)
+{
+	if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255)
+		config_error("Color range should be between 0 and 255");
+	return ;
+}
